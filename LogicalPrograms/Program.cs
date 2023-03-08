2664de9e9de8d79a151ab1f5f087bef4ec9ff9f4
mahesh <lokilokesh1680@gmail.com>
2023-03-08 13:08:34 +0530

    [lokesh] Add:folder is added

---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace LogicalPrograms
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LogicalPrograms");
+        }
+    }
+}

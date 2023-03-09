626d18f6dad52486fcecdb4c583649b553ee81ce
mahesh <lokilokesh1680@gmail.com>
2023-03-09 22:30:19 +0530

    lokesh add:Stopwatch folder is added

--- LogicalPrograms/PerfectNumber.cs
+++ LogicalPrograms/PerfectNumber.cs
@@ -11,7 +11,7 @@ namespace LogicalPrograms
         public void MatchNumb(int num)
         {
             int result = 0;
-            for (int i = 1, i < num; i++)
+            for (int i = 1; i < num; i++)
             {
                 if (num % i == 0)
                     result += 1;
---
+++ LogicalPrograms/StopWatch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class StopWatch
+    {
+        public void run()
+        {
+            const string START = "Start", STOP = "Stop";
+            Console.WriteLine("Enter 'Start to run the stop watch");
+            string start= Console.ReadLine();
+            TimeSpan timespan = TimeSpan.Zero;
+            if (start.Equals(START))
+            {
+                int hour1=timespan.Hours;
+                int minute1=timespan.Minutes;
+                int second1=timespan.Seconds;
+                int millisecond1=timespan.Milliseconds;
+
+                Console.WriteLine("Enter 'Stop' to the end the stop watch");
+                string stop = Console.ReadLine();
+                if (stop.Equals(STOP))
+                {
+                    int hour2 = timespan.Hours;
+                    int minute2 = timespan.Minutes;
+                    int second2 = timespan.Seconds;
+                    int microsecond2 = timespan.Milliseconds;
+                    Console.WriteLine("Stop Watch Time"+(hour2-hour1)+":"+
+                        (minute2-minute1)+":"+(second2-second1)+":"+(millisecond2 - millisecond1));
+                }
+
+            }
+        }
+    }
+}
